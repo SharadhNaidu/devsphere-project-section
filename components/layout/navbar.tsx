c66211ac073aaaa70856f13388010768beb9fcc5
SagarNRao <sagarnrao154@gmail.com>
2024-11-10 14:39:30 +0530

    changed logo on small screen sidebar to devsphere logo

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -96,8 +96,10 @@ export const Navbar = () => {
               <SheetHeader className="mb-4 ml-4">
                 <SheetTitle className="flex items-center">
                   <Link href="/" className="flex items-center">
-                    <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-                    Shadcn
+                    {/* <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" /> */}
+                    <Image src={DevSphereLogo} alt="" width={40} height={40} className="pl-1" />
+                    <div className="p-1"></div>
+                    DevSphere
                   </Link>
                 </SheetTitle>
               </SheetHeader>
